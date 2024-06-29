9183f0599747163daf0a4a2339c584f36d8480ac
mehedihasan2061 <mehedi.hasan206191@gmail.com>
2024-06-30 03:01:42 +0600

    spotCountry added component

--- src/layouts/SpotCountry/SpotCountry.jsx
+++ src/layouts/SpotCountry/SpotCountry.jsx
@@ -4,18 +4,11 @@ import TouristSpot from "../TouristSpot/TouristSpot";
 
 
 const SpotCountry = () => {
-    const loadedCountry = useLoaderData()
-    
+    const loadedCountry = useLoaderData() 
     const { country_name } = useParams()
     const spot_country = country_name;
-    
-   
     const spots = loadedCountry.filter(country => country.country_name === spot_country)
-    console.log(spots);
-    
 
-
-   
     return (
       <div className="container mx-auto grid md:grid-cols-3 gap-6 ">
             {
